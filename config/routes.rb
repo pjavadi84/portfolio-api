0182f0ac2a61ee6f7900168360b8cfb243dfe4ee
Pouya Javadi <90465818+pjavadi@users.noreply.github.com>
2023-10-30 23:26:48 -0700

    created user auth devise layer and projects db layer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :users do
+    resources :projects
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "articles#index"
 end
